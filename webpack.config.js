d70d5e922e14d2fc21b12dc830b0d9fed5db1121
zhmhbest <zhmhbest@gmail.com>
2021-03-23 01:06:04 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const root = path.resolve(__dirname);
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production', // production | development
+    entry: {
+        index: './src/index.ts'
+    },
+    target: 'node',
+    output: {
+        path: path.resolve(root, 'out'),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: ['ts-loader']
+            },
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: 'src/',
+                to: './',
+                globOptions: {
+                    ignore: [
+                        "**/*.ts",
+                        "**/*.js",
+                    ]
+                }
+            }]
+        }),
+    ]
+}
