3f55aade45064cbe3fd9f1f7938830ab90c56a8a
zhmhbest <zhmhbest@gmail.com>
2021-03-25 14:18:20 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-import * as path from 'path';
-console.log("Hello", path.resolve("."));
+import { HttpApp } from './lib/http';
+import { readJSON } from './lib/file';
+
+interface AppConfig {
+    host: string,
+    port: number,
+    on: Array<{dir: string,  prefix: string,}>
+}
+
+const config = readJSON("./config.json") as AppConfig;
+const app = new HttpApp(config.port, config.host);
+for (const it of config.on) {
+    app.onFile(it.prefix, it.dir);
+}
+app.listen();
