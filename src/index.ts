aeca5294b7af00a3f3204c4e68ddb336775b1c93
zhmhbest <zhmhbest@gmail.com>
2021-03-29 09:54:08 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ interface AppConfig {
 }
 const config = readJSON("./config.json") as AppConfig;
 const app = new HttpApp(config.port, config.host);
-app.on(/^\/$/, async (match, req, res) => {
-    res.write('Hi!');
-    res.end();
-});
+// app.on(/^\/$/, async (match, req, res) => {
+//     res.write('Hi!');
+//     res.end();
+// });
 app.onMapping("/maven2", "https://repo1.maven.org/maven2", subPath => {
     if(0 === subPath.length || subPath.endsWith('/')) {
         return undefined;
