b84b1506170792eb87761651b18e8b13f6bb5bdb
zhmhbest <zhmhbest@gmail.com>
2021-04-21 16:26:13 +0800

    readCSVAsync

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -1,10 +1,35 @@
 import * as path from "path";
 import * as fs from "fs";
+import * as co from "co";
+import * as csvParse from "csv-parse";
+
+export const readUTF8Text = (filename: string): string => {
+    const BOM = Buffer.from("\uFEFF"); // EF BB BF
+    let buffer: Buffer = fs.readFileSync(filename, { flag: 'r' });
+    if (BOM.equals(buffer.slice(0, 3))) {
+        // utf-8-sig
+        buffer = buffer.slice(3);
+    }
+    return buffer.toString('utf-8');
+};
 
 export const readJSON = (filename: string): any => {
-    const text = fs.readFileSync(filename, {encoding: 'utf-8', flag: 'r'});
-    return JSON.parse(text);
+    return JSON.parse(readUTF8Text(filename));
 };
+
+export const readCSVAsync = (filename: string) => new Promise((resolve, reject) => {
+    csvParse(readUTF8Text(filename), {
+        comment: '#',
+        delimiter: ',',
+        escape: '\\'
+    }, function (err, records, info) {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(records);
+        }
+    });
+});
 
 export const formatFileSize = (fileSize: number): string => {
     const UNIT = ["B", "K", "M", "G", "T", "P"];
@@ -25,7 +50,7 @@ export const getPureExtensionName = (fileName: string): string => {
 export const moveFile = (originalName: string, newName: string, isDelete?: boolean) => {
     fs.createReadStream(originalName)
         .pipe(fs.createWriteStream(newName));
-    if(undefined === isDelete || isDelete) {
+    if (undefined === isDelete || isDelete) {
         fs.unlinkSync(originalName);
     }
 };
